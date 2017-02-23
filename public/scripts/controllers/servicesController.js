a98dd518d8265be1bc7849b60ee928f1da70d4d3
Brent Reno <brent.reno@gmail.com>
2017-02-23 13:29:32 -0600

    scale up service on click

--- public/scripts/controllers/servicesController.js
+++ public/scripts/controllers/servicesController.js
@@ -2,10 +2,13 @@ myApp.controller("servicesController", ['$scope', function($scope){
   console.log('In servicesController');
   $scope.showInfo = function(idNum){
     console.log("ShowInfo Click");
-    var id = "service-"+idNum;
-    var serviceInfo = angular.element(document.getElementById(id));
+    var containerId = "container-" + idNum;
+    var serviceId = "service-"+idNum;
+    var serviceInfo = angular.element(document.getElementById(serviceId));
+    var container = angular.element(document.getElementById(containerId));
     if(serviceInfo.css("visibility") == "hidden"){
       serviceInfo.removeClass("hidden");
     }
+      container.addClass("scale");
   };
 }]);
