2472f489d078d7635fc0389dc2a880c75ef7fd00
Brent Reno <brent.reno@gmail.com>
2017-02-23 12:54:20 -0600

    show service info click

--- public/scripts/controllers/servicesController.js
+++ public/scripts/controllers/servicesController.js
@@ -1,4 +1,11 @@
 myApp.controller("servicesController", ['$scope', function($scope){
   console.log('In servicesController');
-
+  $scope.showInfo = function(idNum){
+    console.log("ShowInfo Click");
+    var id = "service-"+idNum;
+    var serviceInfo = angular.element(document.getElementById(id));
+    if(serviceInfo.css("visibility") == "hidden"){
+      serviceInfo.removeClass("hidden");
+    }
+  };
 }]);
