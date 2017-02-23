b540ab15af73d5ddea2fcb879b30421ba436e07e
Brent Reno <brent.reno@gmail.com>
2017-02-23 14:35:11 -0600

    show close icon on services scale up

--- public/scripts/controllers/servicesController.js
+++ public/scripts/controllers/servicesController.js
@@ -6,9 +6,12 @@ myApp.controller("servicesController", ['$scope', function($scope){
     var serviceId = "service-"+idNum;
     var serviceInfo = angular.element(document.getElementById(serviceId));
     var container = angular.element(document.getElementById(containerId));
+    var close = angular.element(container.children()[0]);
+
     if(serviceInfo.css("visibility") == "hidden"){
       serviceInfo.removeClass("hidden");
     }
       container.addClass("scale");
+      close.css("visibility", "visible");
   };
 }]);
