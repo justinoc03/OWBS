18475b75840d32987a909c920a2a77810c106823
Brent Reno <brent.reno@gmail.com>
2017-02-24 10:18:36 -0600

    scale service up on click function working

--- public/scripts/controllers/servicesController.js
+++ public/scripts/controllers/servicesController.js
@@ -1,17 +1,35 @@
 myApp.controller("servicesController", ['$scope', function($scope){
   console.log('In servicesController');
-  $scope.showInfo = function(idNum){
+  $scope.showHideInfo = function(idNum, status){
     console.log("ShowInfo Click");
+    var i;
     var containerId = "container-" + idNum;
     var serviceId = "service-"+idNum;
     var serviceInfo = angular.element(document.getElementById(serviceId));
     var container = angular.element(document.getElementById(containerId));
     var close = angular.element(container.children()[0]);
-
-    if(serviceInfo.css("visibility") == "hidden"){
-      serviceInfo.removeClass("hidden");
-    }
+    var serviceDivs = document.getElementsByClassName('col-xs-4');
+    if(status == "show"){
+      for (i = 0; i < serviceDivs.length; i++) {
+        if(serviceDivs[i].id != containerId){
+          console.log(serviceDivs[i]);
+          angular.element(serviceDivs[i]).addClass("hidden");
+        }
+      }
       container.addClass("scale");
-      close.css("visibility", "visible");
+      serviceInfo.removeClass("hidden");
+      close.removeClass("hidden").addClass("visible");
+    }
+    if(status == "hide"){
+      close.removeClass("visible").addClass("hidden");
+      serviceInfo.addClass("hidden");
+      for (i = 0; i < serviceDivs.length; i++) {
+        if(serviceDivs[i].id != containerId){
+          console.log(serviceDivs[i]);
+          angular.element(serviceDivs[i]).removeClass("hidden").addClass("visible");
+        }
+      }
+      container.removeClass("scale");
+    }
   };
 }]);
