c500891fdb75bb3da7ae1cbe47fc01a416dbaded
Brent Reno <brent.reno@gmail.com>
2017-02-28 13:19:04 -0600

    x-axis flip animation for staff profiles

--- public/scripts/controllers/staffController.js
+++ public/scripts/controllers/staffController.js
@@ -2,4 +2,9 @@ myApp.controller("staffController", ['$scope', function($scope){
   console.log('In staffController');
   // set footer position for page
   angular.element(document.getElementById("footerSection")).css("position","fixed");
+  $scope.staffToggle = function(staffId){
+      var staffNum = "staff-" + staffId;
+      var staffMember = angular.element(document.getElementById(staffNum));
+      staffMember.toggleClass('flipped');
+  };
 }]);
