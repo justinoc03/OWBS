31f79ad949dc479017faff5282ab78bd491ea6a4
Justin O'Connor <justin.oc03@gmail.com>
2017-03-07 11:44:25 -0600

    updated DB design

--- server/databaseDesign.sql
+++ server/databaseDesign.sql
@@ -1,7 +1,7 @@
 CREATE TABLE jobPostings (
 jobPosting_id SERIAL PRIMARY KEY NOT NULL,
 jobPosting_name VARCHAR(200),
-jobPosting_description VARCHAR(500),
+jobPosting_description TEXT,
 jobPosting_open BOOLEAN,
 jobPosting_start timestamp(0)
 );
@@ -9,3 +9,6 @@ jobPosting_start timestamp(0)
 
 INSERT INTO jobPostings (jobPosting_name, jobPosting_description, jobPosting_open, jobPosting_start) VALUES ('Tower Dude', 'Climbs towers and builds them up', TRUE, now());
 INSERT INTO jobPostings (jobPosting_name, jobPosting_description, jobPosting_open, jobPosting_start) VALUES ('Machine guy', 'Keeps machines running', FALSE, now());
+INSERT INTO jobPostings (jobPosting_name, jobPosting_description, jobPosting_open, jobPosting_start) VALUES ('Rebar Bender', 'Bending rebar, of course!', TRUE, now());
+INSERT INTO jobPostings (jobPosting_name, jobPosting_description, jobPosting_open, jobPosting_start) VALUES ('HR Person', 'Makes sure to hire good people', FALSE, now());
+INSERT INTO jobPostings (jobPosting_name, jobPosting_description, jobPosting_open, jobPosting_start) VALUES ('Driver', 'Gets everyone to the jobsite safely and on time', TRUE, now());
