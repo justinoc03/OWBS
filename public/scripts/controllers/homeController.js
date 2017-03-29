13a687cb64c35435940efc944cd9a8eff59c7bb9
Justin O'Connor <justin.oc03@gmail.com>
2017-03-29 12:12:43 -0500

    cleanse inputs is built

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -1,5 +1,11 @@
-myApp.controller("homeController", ['$scope', function($scope){
+myApp.controller("homeController", ['$scope', '$http', '$location', function($scope, $http, $location){
   console.log('In homeController');
+
+  $scope.contactUsButton = function() {
+    $location.path('/contactUs');
+  };
+
+
   // set footer position for page
   angular.element(document.getElementById("footerSection")).css("position","fixed");
 }]);
