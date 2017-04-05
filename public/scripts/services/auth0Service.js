26e30b4cfcb3d9a1daa9c5dc37b7ebed06a58e50
Justin O'Connor <justin.oc03@gmail.com>
2017-04-05 16:37:25 -0500

    adminEditCareersController is now properly set up with authorization for admin only

--- public/scripts/services/auth0Service.js
+++ public/scripts/services/auth0Service.js
@@ -1,52 +1,75 @@
-myApp.service('authService', ['$http', '$q', 'lock', 'authManager', function($http, $q, lock, authManager){
+myApp.service('authService', ['$http', '$q', 'lock', 'authManager', '$rootScope', function($http, $q, lock, authManager, $rootScope){
 
-  var deferredProfile = $q.defer();
+  var userProfile = JSON.parse(localStorage.getItem('profile')) || null;
+     var deferredProfile = $q.defer();
 
-  function login() {
-    lock.show();
-  }
+     if (userProfile) {
+       deferredProfile.resolve(userProfile);
+     }
 
-  // Logging out just requires removing the user's
-  // id_token and profile
-  function logout() {
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('profile');
-    authManager.unauthenticate();
-  }
+     function login() {
+       lock.show();
+     }
 
-  // Set up the logic for when a user authenticates
-  // This method is called from app.run.js
-  function registerAuthenticationListener() {
-    lock.on('authenticated', function (authResult) {
+     // Logging out just requires removing the user's
+     // id_token and profile
+     function logout() {
+       deferredProfile = $q.defer();
+       localStorage.removeItem('id_token');
+       localStorage.removeItem('profile');
+       authManager.unauthenticate();
+       userProfile = null;
+     }
 
-      lock.getProfile(authResult.idToken, function (error, profile) {
-          if (error) {
-            return console.log(error);
-          }
-          localStorage.setItem('profile', JSON.stringify(profile));
-          deferredProfile.resolve(profile);
-          
-          var profileParsed = JSON.parse(localStorage.getItem('profile'));
-          console.log(profileParsed);
-        });
+     // Set up the logic for when a user authenticates
+     // This method is called from app.run.js
+     function registerAuthenticationListener() {
+       lock.on('authenticated', function (authResult) {
+         localStorage.setItem('id_token', authResult.idToken);
+         authManager.authenticate();
 
+         lock.getProfile(authResult.idToken, function (error, profile) {
+           if (error) {
+             return console.log(error);
+           }
 
-      localStorage.setItem('id_token', authResult.idToken);
-      authManager.authenticate();
+           localStorage.setItem('profile', JSON.stringify(profile));
+           userProfile = profile;
+           deferredProfile.resolve(profile);
+         });
 
-    });
+       });
 
-    lock.on('authorization_error', function (err) {
-        console.log(err);
-      });
-  }
+       lock.on('authorization_error', function (err) {
+         console.log(err);
+       });
+     }
 
-  return {
-    login: login,
-    logout: logout,
-    registerAuthenticationListener: registerAuthenticationListener
-  };
+     function getProfileDeferred() {
+       return deferredProfile.promise;
+     }
 
+     function isAdmin() {
+       return userProfile && userProfile.app_metadata
+         && userProfile.app_metadata.roles
+         && userProfile.app_metadata.roles.indexOf('admin') > -1;
+     }
+
+     $rootScope.$on('$stateChangeStart', function(event, nextRoute) {
+       if (nextRoute.controller === 'adminEditCareersController') {
+         if (!isAdmin()) {
+           alert('You are not allowed to see the Admin content');
+           return event.preventDefault();
+         }
+       }
+     });
+
+     return {
+       login: login,
+       logout: logout,
+       registerAuthenticationListener: registerAuthenticationListener,
+       getProfileDeferred: getProfileDeferred
+     };
 
 }]);//end authService
 
