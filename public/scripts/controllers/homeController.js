6027ca8b6e4a723aa950994d61633675ffd3a7dc
Brent Reno <brent.reno@gmail.com>
2017-06-24 10:46:29 -0500

    saturday morning redo

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -9,7 +9,7 @@ myApp.controller("homeController", ['$scope', '$http', '$location', function($sc
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 7000,
     arrows: false,
     pauseOnHover: false,
     pauseOnFocus: false
