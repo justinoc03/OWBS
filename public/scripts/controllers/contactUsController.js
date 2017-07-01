c78d1358e348000a11c97532ed83b6df8a7bb586
Justin O'Connor <justin.oc03@gmail.com>
2017-07-01 13:59:48 -0500

    contact page built out, needs sendgrid routes

--- public/scripts/controllers/contactUsController.js
+++ public/scripts/controllers/contactUsController.js
@@ -1,6 +1,10 @@
 myApp.controller("contactUsController", ['$scope', '$http', '$location',"$window", function($scope, $http, $location, $window){
   console.log('In contactUsController');
   $window.scrollTo(0, 0);
+
+
+
+
   // set footer position for page
-  angular.element(document.getElementById("footerSection")).css("position","fixed");
+  angular.element(document.getElementById("footerSection")).css("position","static");
 }]);
